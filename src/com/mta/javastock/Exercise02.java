a0482500942d880b284c3288889d96ef43fdce3b
michal <michal@michal-pc>
2015-04-05 21:56:55 +0300

    Ex 02

---
+++ src/com/mta/javastock/Exercise02.java
@@ -0,0 +1,23 @@
+package com.mta.javastock;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Exercise02 extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		int num1;
+		int num2;
+		int num3;
+		
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		int result = (num1+num2)* num3;
+		
+		String resultStr = new String ("<h1>result of "+num1+"*"+num2+"="+result+"</h1>");
+		
+		resp.setContentType("text/html");
+		resp.getWriter().println(resultStr);
+	}
+}
