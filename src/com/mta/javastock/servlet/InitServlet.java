69110cba97c8924ed29bc334427c25ece3be3e30
michal <michal@michal-pc>
2015-06-03 21:08:01 +0300

    ex 08

---
+++ src/com/mta/javastock/servlet/InitServlet.java
@@ -0,0 +1,16 @@
+package com.mta.javastock.servlet;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.algo.service.ServiceManager;
+
+import com.mta.javastock.service.PortfolioManager;
+@SuppressWarnings("serial")
+public class InitServlet extends javax.servlet.http.HttpServlet {
+
+	public void init() throws ServletException {
+		PortfolioManager pm = new PortfolioManager();
+		ServiceManager.setPortfolioManager(pm);
+	}
+}
